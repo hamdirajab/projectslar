40d76d94b5690ffd934b9965ba31c8fc1a1c217a
hamdi <hamdi_1_4_97@hotmail.com>
2018-06-11 18:25:17 +0300

    user pages working expact security

--- app/Photo.php
+++ app/Photo.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
 {
+    protected $upload = '/image/';
+
     protected $fillable = ['file'];
+
+    public function getFileAttribute($photo){
+
+        return $this->upload . $photo;
+
+    }
+
 }
